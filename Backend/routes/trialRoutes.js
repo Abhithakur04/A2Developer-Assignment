ce332cdbbcb5f254c5c485645a914867edb53bf3
Abhithakur04 <152187846+Abhithakur04@users.noreply.github.com>
2025-06-05 11:53:43 +0530

    Update trialRoutes.js

--- Backend/routes/trialRoutes.js
+++ Backend/routes/trialRoutes.js
@@ -1,9 +1,30 @@
 const express = require('express');
-const { submitTrial } = require('../controllers/trialController');
 
 const router = express.Router();
 
-router.post('/', submitTrial);
+router.post('/api/trial', async (req, res) => {
+  try {
+    const { name, email, phone, message } = req.body;
+
+
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({ message: 'Invalid email address' });
+    }
+
+   
+    if (!validator.isMobilePhone(phone, 'en-IN')) {
+      return res.status(400).json({ message: 'Invalid phone number' });
+    }
+
+    const trial = new Trial({ name, email, phone, message });
+    await trial.save();
+
+    res.status(201).json({ message: 'Trial form submitted successfully' });
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ error: `Server error ${err}` });
+  }
+});
 
 module.exports = router;
 
